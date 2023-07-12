b9dde6c5c9ed0d66d65880573999ffc7614eade4
Charlessnow <ekekwecharles50@gmail.com>
2023-07-12 11:58:53 -0700

    Base Class defining common attr/methods

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""BaseModel class: defines all common attributes/methods for other classes"""
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """Base class"""
+
+    def __init__(self):
+        """Initializes a new instance of the BaseModel class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """Updates the `updated_at` attribute with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary representation of the instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        return "[{}] ({}) {}".\
+                format(self.__class__.__name__, self.id, self.__dict__)
